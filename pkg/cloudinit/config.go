83622c372bcf74babb48a8d32244bdfaf4d3b13e
Fred78290 <frederic.boltz@gmail.com>
2023-05-14 11:22:55 +0200

    FEAT: Export BuildCloudInit

---
+++ pkg/cloudinit/config.go
@@ -0,0 +1,10 @@
+package cloudinit
+
+import (
+	"github.com/Fred78290/kubernetes-desktop-autoscaler/desktop"
+)
+
+// BuildCloudInit build map for guestinfo
+func BuildCloudInit(hostName string, userName, authKey string, cloudInit interface{}, network *desktop.Network, nodeIndex int) (desktop.GuestInfos, error) {
+	return desktop.BuildCloudInit(hostName, userName, authKey, cloudInit, network, nodeIndex)
+}
